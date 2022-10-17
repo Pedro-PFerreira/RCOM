c3c1fe1b0665a422b0b8d2811c048425855ba9cb
Pedro Ferreira <80838342+Pedro-PFerreira@users.noreply.github.com>
2022-10-17 16:10:18 +0100

    Separated code to respective functions

--- lab1/src/link_layer.c
+++ lab1/src/link_layer.c
@@ -4,21 +4,27 @@
 #include "state_machine.h"
 #include "macros.h"
 #include "alarm.c"
-
-#define _POSIX_SOURCE 1 // POSIX compliant source
-// MISC
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
 
 int state = 0;
 char* filename;
 int nTries = 0;
 
-int llopent(int fd){
-    //TODO
-}
+unsigned char rcv_buf[MAX_SIZE + 1] = {0};
+unsigned char send_buf[MAX_SIZE + 1] = {0};
 
-int llopenr(int fd){
-    //TODO
-}
+int bytes = 0;
+
+int fd;
+
+volatile int STOP = FALSE;
 
 ////////////////////////////////////////////////
 // LLOPEN
@@ -26,19 +32,66 @@ int llopenr(int fd){
 int llopen(LinkLayer connectionParameters)
 {   
 
-    set_state(state, FLAG_RCV);
+    struct termios oldtio;
+    struct termios newtio;
 
-    
-}
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
 
-////////////////////////////////////////////////
-// LLWRITE
-////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
-{
-    // TODO
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    int state = START;
+
+
+    write(fd, rcv_buf, bytes);
+    printf("Sent: %s:%d\n", send_buf, bytes);
+    // The while() cycle should be changed in order to respect the specifications
+    // of the protocol indicated in the Lab guide
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    llclose(fd);
 
     return 0;
+    
 }
 
 unsigned char stuff(unsigned char * block){
@@ -65,26 +118,52 @@ unsigned char destuff(unsigned char * block){
 }
 
 ////////////////////////////////////////////////
+// LLWRITE
+////////////////////////////////////////////////
+int llwrite(unsigned char *buf, int bufSize)
+{       
+    unsigned char buf[MAX_SIZE + 1] = {0};
+    unsigned char rcv_buf[MAX_SIZE + 1] = {0};
+    unsigned char send_buf[MAX_SIZE + 1] = {0};
+    rcv_buf[0] = FLAG_RCV;
+    rcv_buf[1] = A_RCV;
+    rcv_buf[2] = C_RCV;
+    rcv_buf[3] = A_RCV ^ C_RCV;
+    rcv_buf[4] = FLAG_RCV;
+
+    for (int i = 0; i < bufSize; i++){
+        buf[i] = stuff(buf[i]);
+    }
+    
+
+    return 0;
+}
+
+
+
+////////////////////////////////////////////////
 // LLREAD
 ////////////////////////////////////////////////
 int llread(unsigned char *packet)
 {
-    *packet = stuff(packet);
 
     int packet_sz = *packet / sizeof(unsigned char);
 
     *packet = destuff(packet);
     
-    if (packet_sz > MAX_SIZE) {
-        printf("Can't read packet!\n");
-        return 1;
-    }
-    unsigned char buf[packet_sz - 1];
+    while(TRUE){
+        if (packet_sz > MAX_SIZE) {
+            printf("Can't read packet!\n");
+            return 1;
+        }
+        unsigned char buf[packet_sz - 1];
 
-    for (int i = 0; i < packet_sz; i++){
-        packet[i]= buf[i];
+        for (int i = 0; i < packet_sz; i++){
+            packet[i]= buf[i];
+        }
+
+        break;       
     }
-    
 
     return packet_sz;
 }
